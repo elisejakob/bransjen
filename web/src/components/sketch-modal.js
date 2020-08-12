1685fe1e6d16d8430a7e8dd46a95501b3db0dc45
elisejakob <hei@elisejakob.no>
2020-08-12 23:24:02 +0200

    sketch modal progress

--- web/src/components/sketch-modal.js
+++ web/src/components/sketch-modal.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import {cn, buildImageObj} from '../lib/helpers'
 import {imageUrlFor} from '../lib/image-url'
+import styles from './sketch-modal.module.css'
 
 function SketchModal (props) {
   return (
-    <div>
-      :)
+    <div className={styles.modal}>
+      <div className={styles.gallery}>
+        {props.currentSketch.mainImage && (
+          <img
+            src={imageUrlFor(buildImageObj(props.currentSketch.mainImage))
+              .width(600)
+              .url()}
+            alt={props.currentSketch.mainImage.alt}
+            className={styles.image}
+          />
+        )}
+      </div>
     </div>
   )
+}
+
+SketchModal.defaultProps = {
+  currentSketch: ''
 }
 
 export default SketchModal
--- web/src/components/sketchbook.js
+++ web/src/components/sketchbook.js
@@ -1,5 +1,5 @@
 import {Link} from 'gatsby'
-import React from 'react'
+import React, { useState } from 'react'
 import {cn, buildImageObj} from '../lib/helpers'
 import {imageUrlFor} from '../lib/image-url'
 import SketchModal from './sketch-modal'
@@ -7,12 +7,21 @@ import SketchModal from './sketch-modal'
 import styles from './sketchbook.module.css'
 
 function Sketchbook (props) {
+  let [showModal, toggleModal] = useState(false);
+  let [currentSketch, setCurrentSketch] = useState(0);
+  let handleModal = (index) => {
+    toggleModal(showModal = true);
+    setCurrentSketch(index);
+  }
+  let closeModal = (index) => {
+    toggleModal(showModal = false);
+  }
   return (
     <div className={styles.root}>
       <ul className={styles.grid}>
         {props.nodes &&
-          props.nodes.map(node => (
-            <li className={styles.sketchthumb} key={node.id}>
+          props.nodes.map((node, index) => (
+            <li className={styles.sketchthumb} key={node.id} onClick={() => handleModal(index)}>
               {node.mainImage && node.mainImage.asset && (
                 <img
                   src={imageUrlFor(buildImageObj(node.mainImage))
@@ -24,7 +33,18 @@ function Sketchbook (props) {
             </li>
           ))}
       </ul>
-      <SketchModal />
+      { showModal &&
+        <>
+          <SketchModal currentSketch={props.nodes[currentSketch]} />
+          <div className={styles.modalNav}>
+            {currentSketch > 0 && <div className={styles.modalNavLeft} onClick={() => handleModal(currentSketch-1)}>&larr;</div> }
+            {currentSketch < props.nodes.length-1 && <div className={styles.modalNavRight} onClick={() => handleModal(currentSketch+1)}>&rarr;</div> }
+          </div>
+          <div className={styles.closeModal} onClick={closeModal}>
+            <div className={styles.closeModal__icon}></div>
+          </div>
+        </>
+      }
     </div>
   )
 }
