df86d474e7d675c43e8214f733a1cbf254ad06a0
elisejakob <hei@elisejakob.no>
2020-08-19 00:07:55 +0200

    add project category filter

--- web/src/components/project-preview-grid.js
+++ web/src/components/project-preview-grid.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectPreview from './project-preview'
 
 import styles from './project-preview-grid.module.css'
 
 function ProjectPreviewGrid (props) {
+  let [currentCategory, setCategory] = useState("all");
   return (
     <div className={styles.root}>
       <nav className={styles.categories}>
         <ul className={styles.categoryFilter}>
           {props.categories &&
             props.categories.map(category => (
-              <li className={styles.categoryFilterButton} key={category.id}>
+              <li
+                className={styles.categoryFilterButton}
+                key={category.id}
+                onClick={() => setCategory(category.title)}
+                role="button"
+              >
                 {category.title}
               </li>
             ))}
@@ -18,7 +24,17 @@ function ProjectPreviewGrid (props) {
       </nav>
       <ul className={styles.grid}>
         {props.nodes &&
-          props.nodes.map(node => (
+          props.nodes.filter(node => {
+            if (!node.categories.length) {
+              return false
+            }
+            if (currentCategory === "all") {
+              return true
+            }
+            return node.categories.some(category => {
+              return currentCategory === category.title
+            })
+          }).map(node => (
             <li key={node.id}>
               <ProjectPreview {...node} />
             </li>
